74160c7a1e10e3f7d7c1050c18e77b40a1269417
sheptianbagja <sheptianbagjautama@gmail.com>
2020-11-14 17:08:52 +0700

    Init projek, Migration, Model, dan init redirect snap midtrans token

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function create(Request $request)
+    {
+    }
+
+    private function getMidtransSnapUrl($params)
+    {
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_PRODUCTION');
+        \Midtrans\Config::$is3ds = (bool) env('MIDTRANS_3DS');
+
+        $snapUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+        return $snapUrl;
+    }
+}
---
+++ app/PaymentLog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentLog extends Model
+{
+    protected $table = 'payment_logs';
+
+    protected $fillable = [
+        'status', 'payment_type', 'raw_response', 'order_id'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+    ];
+}
